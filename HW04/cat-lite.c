fac6f367eed72c2b8dac7a295ccf2cbcfa662da9
mluzarow <mluzarow@purdue.edu>
2014-02-10 04:44:17 -0500

    cat

--- HW04/cat-lite.c
+++ HW04/cat-lite.c
@@ -1,1 +1,24 @@
-cat -
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+     FILE *file;
+     char file_name[255];
+     char ch;
+
+     file = fopen(file_name, "r");
+     
+     if (file == NULL) {
+       printf("Doesn't exist\n");
+       return(EXIT_SUCCESS);
+     }
+
+     while((ch == fgetc(file)) != EOF) {
+       putchar(ch);
+     }
+
+     fclose(file);
+       
+     return(EXIT_SUCCESS);
+}
