27e3d6d5b614af963e8f7bf061cc85e206218aae
mluzarow <mluzarow@purdue.edu>
2014-02-10 16:17:18 -0500

    loops forever

--- HW04/cat-lite.c
+++ HW04/cat-lite.c
@@ -6,19 +6,28 @@ int main(int argc, char ** argv) {
      FILE *file;
      char file_name[255];
      char ch;
+     int counter = 0;
 
-     file = fopen(file_name, "r");
+     if (argc < 1) {
+          printf("Usage: ./cat-lite <filename>\n");
+	  return(EXIT_SUCCESS);
+     }
+
+     for (counter = 1; counter < argc; counter++) {
+          strncpy(file_name, argv[counter], 255);  
+
+	  file = fopen(file_name, "r");
      
-     if (file == NULL) {
-       printf("Doesn't exist\n");
-       return(EXIT_SUCCESS);
-     }
+	  if (file == NULL) {
+	       printf("Cannot find file: %s\n", file_name);
+	       return(EXIT_SUCCESS);
+	  }
 
-     while((ch == fgetc(file)) != EOF) {
-       putchar(ch);
-     }
+	  while((ch == fgetc(file)) != EOF) {
+	       putchar(ch);
+	  }
 
-     fclose(file);
-       
+	  fclose(file);
+     }       
      return(EXIT_SUCCESS);
 }
