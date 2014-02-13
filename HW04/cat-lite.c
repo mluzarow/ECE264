f568b69566e54ad8a73e8cf8e24b4a2ddc810103
mluzarow <mluzarow@purdue.edu>
2014-02-12 21:41:53 -0500

    fixed line reading

--- HW04/cat-lite.c
+++ HW04/cat-lite.c
@@ -4,30 +4,39 @@
 
 int main(int argc, char ** argv) {
      FILE *file;
-     char file_name[255];
+     char file_name[255]; //Max file name size
      char ch;
      int counter = 0;
 
-     if (argc < 1) {
+     //If the only input is the program command "./cat-lite", ask
+     // for a file name
+     if (argc <= 1) {
           printf("Usage: ./cat-lite <filename>\n");
 	  return(EXIT_SUCCESS);
      }
 
+     //For loop capturing multiple files to read
      for (counter = 1; counter < argc; counter++) {
           strncpy(file_name, argv[counter], 255);  
 
-	  file = fopen(file_name, "r");
+	  file = fopen(file_name, "r"); //r - reading
      
+	  //Invalid filename is enetered
 	  if (file == NULL) {
 	       printf("Cannot find file: %s\n", file_name);
 	       return(EXIT_SUCCESS);
 	  }
 
-	  while((ch == fgetc(file)) != EOF) {
+	  //Output to console as long as the is no EOF symbol
+	  /*while((ch == fgetc(file)) != EOF) {
+	       putchar(ch);
+	       }*/
+	  while (!feof(file)) {
+	       ch = fgetc(file);
 	       putchar(ch);
 	  }
 
-	  fclose(file);
+	  fclose(file); //close steam
      }       
      return(EXIT_SUCCESS);
 }
