f0bc75ac9bfa530e79225f848d374832eb586f04
mluzarow <mluzarow@purdue.edu>
2014-02-12 21:41:29 -0500

    final

--- HW04/echo-lite.c
+++ HW04/echo-lite.c
@@ -4,6 +4,8 @@
 int main(int argc, char ** argv) {
      int counter = 0;
 
+     //Since the first value in the argv array is the program
+     // command "./echo-lite", skip that part of the array.
      for (counter = 1; counter < (argc - 1); counter++) {
           printf("%s ", argv[counter]);
      }
