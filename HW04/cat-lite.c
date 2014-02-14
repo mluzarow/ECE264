f27c358b0a8e9c1f6e488fcb2b4014640e2f2a4b
mluzarow <mluzarow@purdue.edu>
2014-02-14 02:56:08 -0500

    should be g2g

--- HW04/cat-lite.c
+++ HW04/cat-lite.c
@@ -5,11 +5,8 @@
 int main(int argc, char ** argv) {
      FILE *file;
      char file_name[255]; //Max file name size
-     //char ch;
      char buffer[255];
      int counter = 0;
-
-     //printf("Current argc value: %d\n", argc);
      
      //If the only input is the program command "./cat-lite", ask
      // for a file name
@@ -18,25 +15,21 @@ int main(int argc, char ** argv) {
      }
 
      for (counter = 1; counter < argc; counter ++) {
-       //   printf("%s\n", argv[counter]);   
        
           if (strcmp(argv[counter], "--help") == 0) {
 	       printf("Usage: cat-lite [--help] [FILE]...\nWith no FILE, or when FILE is -, read standard input.\n\nExamples:\n  cat-lite README   Print the file README to standard output.\n  cat-lite f - g    Print f's contents, then standard input, \n                    then g's contents.\n  cat-lite          Copy standard input to standard output.\n\n");
 	       return(EXIT_SUCCESS);
-	  } //else if (strcmp(argv[counter], "-") == 0) {
-	  // fprintf(stdin);
-	  //}
+	  } 
      }
 
      //For loop capturing multiple files to read
      for (counter = 1; counter < argc; counter++) {
           if (strcmp(argv[counter], "-") == 0) {
-	    while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-		    size_t bytes = fread(buffer, 1, sizeof(buffer), stdin);
-		    fwrite(buffer, 1, bytes, stdout);
+	       while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+		    if (buffer != NULL) {
+		         printf("%s", buffer);
+		    }
 	       }
-	       
-	       //fprintf(stin);
 	  } else {
 
 	       strncpy(file_name, argv[counter], 255);  
