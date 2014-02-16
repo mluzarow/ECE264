c5d690d107ab9029e2fdc9d5ee095edce478e377
mluzarow <mluzarow@purdue.edu>
2014-02-16 18:18:26 -0500

    initial

--- HW05/answer05.c
+++ HW05/answer05.c
@@ -1,1 +1,47 @@
-answer
+/**
+ * Loads a bmp (windows bitmap) image, returning an Image structure.
+ * Will return NULL if there is any error.
+ *
+ * Note: This function is written for you and appears in image-bmp.c
+ */
+Image * Image_loadbmp(const char * filename);
+
+/**
+ * Saves an Image structure to a file. Returns TRUE if success, or
+ * FALSE if there is any error.
+ *
+ * Note: This function is written for you and appears in image-bmp.c
+ */
+int Image_savebmp(const char * filename, Image * image);
+
+/**
+ * Loads an ECE264 image file, returning an Image structure.
+ * Will return NULL if there is any error.
+ *
+ * Hint: Please see the README for extensive hints
+ */
+Image * Image_load(const char * filename);
+
+/**
+ * Save an image to the passed filename, in ECE264 format.
+ * Return TRUE if this succeeds, or FALSE if there is any error.
+ *
+ * Hint: Please see the README for extensive hints
+ */
+int Image_save(const char * filename, Image * image);
+
+/**
+ * Free memory for an image structure
+ *
+ * Image_load(...) (above) allocates memory for an image structure. 
+ * This function must clean up any allocated resources. If image is 
+ * NULL, then it does nothing. (There should be no error message.) If 
+ * you do not write this function correctly, then valgrind will 
+ * report an error. 
+ */
+void Image_free(Image * image);
+
+/**
+ * Performs linear normalization, see README
+ */
+void linearNormalization(int width, int height, uint8_t * intensity);
