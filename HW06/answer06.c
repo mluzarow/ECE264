4c3ab9cfaaec579a1d7cd8fe4fdfe30dc0237798
mluzarow <mluzarow@purdue.edu>
2014-02-22 15:46:22 -0500

    initial

---
+++ HW06/answer06.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "answer06.h"
+
+void partitionAll(int value) {
+
+
+}
+
+void partitionIncreasing(int value) {
+
+
+}
+
+void partitionDecreasing(int value) {
+
+
+}
+
+void partitionOdd(int value) {
+
+
+}
+
+void partitionEven(int value) {
+
+
+}
+
+void partitionOddAndEven(int value) {
+
+
+}
+
+void partitionPrime(int value) {
+
+
+}
