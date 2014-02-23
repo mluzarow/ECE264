ef41e9ff66557575c0a53c81e3aef7244fa660e7
mluzarow <mluzarow@purdue.edu>
2014-02-23 01:23:37 -0500

    +partitionAll

--- HW06/answer06.c
+++ HW06/answer06.c
@@ -4,9 +4,48 @@
 
 #include "answer06.h"
 
+//prototypes
+void intHelper(int, int, int*, int);
+void intPrinter(int *, int);
+
+void intPrinter(int * buffer, int size) {
+  int i = 0;
+
+  printf("=");
+
+  for (i = 0; i <= size; i++) {
+    printf("%d", buffer[i]);
+    if (i != size) {
+      printf("+");
+    }
+  }
+
+  printf("\n");
+}
+
+void intHelper(int value, int sum, int * buffer, int depth) {
+     while (sum != value) { 
+          buffer[depth] += 1;
+	  sum++;
+
+	  if (sum == value) {
+               intPrinter(buffer, depth);
+	       /*if (depth != 0) {
+		    sum -= buffer[depth];
+		    }*/
+	  } else if (sum < value) { //Sum != value so another # can come after
+	       intHelper(value, sum, buffer, depth + 1);
+	  }
+     }
+     buffer[depth] = 0;
+}
+
 void partitionAll(int value) {
+     int * buffer = malloc(sizeof(int) * value);
+     
 
-
+     intHelper(value, 0, buffer, 0);
+     free(buffer);
 }
 
 void partitionIncreasing(int value) {
