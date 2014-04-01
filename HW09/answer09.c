5891f2ccc050fc7f137b1f3735e0537fff79f938
mluzarow <mluzarow@purdue.edu>
2014-04-01 01:34:18 -0400

    +a lot of stuff

--- HW09/answer09.c
+++ HW09/answer09.c
@@ -31,3 +31,78 @@ void HuffNode_destroy(HuffNode * tree) {
 
      tree = NULL;
 }
+
+Stack * Stack_create() {
+     Stack * temp = malloc(sizeof(Stack));
+     
+     temp->head = NULL;
+
+     return(temp);
+}
+
+void Stack_destroy(Stack * stack) {
+     if (stack == NULL) {
+          return;
+     }
+
+     
+}
+
+int Stack_isEmpty(Stack * stack) {
+     if (stack->head == NULL) {
+          return(1);
+     } else {
+          return(0);
+     }
+}
+
+HuffNode * Stack_popFront(Stack * stack) {
+
+  return(NULL);
+}
+
+void Stack_pushFront(Stack * stack, HuffNode * tree) {
+
+
+}
+
+void Stack_popPopCombinePush(Stack * stack) {
+
+
+}
+
+HuffNode * HuffTree_readTextHeader(FILE * fp) {
+     char * test;
+     int t = 0;
+     char temp_char;
+     HuffNode * final = NULL;
+     int ppc = 0;
+     int push = 0;
+
+     while ((temp_char = fgetc(fp)) != EOF) {
+          test[t] = temp_char;
+	  t++;
+     }
+
+     printf("The Header is: %s\n", test);
+
+     fclose(fp);
+
+     t = 0;
+     while (test[t] != '2') {
+       // printf("%c: NOT 2\n", test[t]);
+       if (test[t] == '0') { //pop pop combine
+	    
+       }  else if (test[t] == '1') { //put on stack
+	    
+       }
+       t = t + 2;
+     }
+
+     return(final);
+}
+
+HuffNode * HuffTree_readBinaryHeader(FILE * fp) {
+
+  return(NULL);
+}
