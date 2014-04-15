554afe05514c746a48dc67091ee98efd025b3d59
mluzarow <mluzarow@purdue.edu>
2014-04-15 02:38:27 -0400

    fixed

--- HW11/answer11.c
+++ HW11/answer11.c
@@ -1,4 +1,3 @@
-
 #include "answer11.h"
 
 #include <stdio.h>
@@ -21,49 +20,36 @@ int isValidState(const char * state) {
 	       dups[0] = 1;
 	  } else if (state[i] == '2') {
 	       dups[1] = 1;
+	  }  else if (state[i] == '3') {
+	       dups[2] = 1;
+	  }  else if (state[i] == '4') {
+	       dups[3] = 1;
+	  }  else if (state[i] == '5') {
+	       dups[4] = 1;
+	  }  else if (state[i] == '6') {
+	       dups[5] = 1;
+	  }  else if (state[i] == '7') {
+	       dups[6] = 1;
+	  }  else if (state[i] == '8') {
+	       dups[7] = 1;
+	  }  else if (state[i] == '9') {
+	       dups[8] = 1;
+	  }  else if (state[i] == 'A') {
+	       dups[9] = 1;
+	  }  else if (state[i] == 'B') {
+	       dups[10] = 1;
+	  }  else if (state[i] == 'C') {
+	       dups[11] = 1;
+	  }  else if (state[i] == 'D') {
+	       dups[12] = 1;
+	  }  else if (state[i] == 'E') {
+	       dups[13] = 1;
+	  }  else if (state[i] == 'F') {
+	       dups[14] = 1;
+	  }  else if (state[i] == '-') {
+	       dups[15] = 1;
 	  }
-       case('3'):
-	    dups[2] = 1;
-	    break;
-       case('4'):
-	    dups[3] = 1;
-	    break;
-       case('5'):
-	    dups[4] = 1;
-	    break;
-       case('6'):
-	    dups[5] = 1;
-	    break;
-       case('7'):
-	    dups[6] = 1;
-	    break;
-       case('8'):
-	    dups[7] = 1;
-	    break;
-       case('9'):
-	    dups[8] = 1;
-	    break;
-       case('A'):
-	    dups[9] = 1;
-	    break;
-       case('B'):
-	    dups[10] = 1;
-	    break;
-       case('C'):
-	    dups[11] = 1;
-	    break;
-       case('D'):
-	    dups[12] = 1;
-	    break;
-       case('E'):
-	    dups[13] = 1;
-	    break;
-       case('F'):
-	    dups[14] = 1;
-	    break;
-       case('-'):
-	    dups[15] = 1;
-       }
+     }
        
        for (i = 0; i < 16; i++) {
 	    if (dups[i] != 1) {
