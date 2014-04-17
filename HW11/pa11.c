93601b656124e39abb00b3a9b0fa73f8a3f4fe44
mluzarow <mluzarow@purdue.edu>
2014-04-16 21:02:35 -0400

    stages

--- HW11/pa11.c
+++ HW11/pa11.c
@@ -9,28 +9,50 @@ int main (int argc, char ** argv) {
      //argv[1] = stage
      //argv[2] = state
      //argv[3] = moves
+     if (argv[1][0] == '1') {
+          if (argc != 4) {
+	       printUsage();
+	       return(EXIT_FAILURE);
+	  }
+	  /*
+	  if ((argv[1][0] != '1') && (argv[1][0] != '2') && (argv[1][0] != '3')) {
+	       printf("Incorrect stage value.\n");
+	       return(EXIT_FAILURE);
+	  } 
+	  */
+	  if (isValidState(argv[2]) == 0) {
+	       printf("The state is not valid.\n\n");
+	       return(EXIT_FAILURE);
+	  }
+       
+	  if (isValidMoveList(argv[3]) == 0) {
+	       printf("The move list is not valid.\n");
+	       return(EXIT_FAILURE);
+	  }
+	  
+	  processMoveList(argv[2], argv[3]);
+     } else if (argv[1][0] == '2') {
+          if (argc != 4) {
+	       printUsage();
+	       return(EXIT_FAILURE);
+	  }
 
-     if (argc != 4) {
-          printUsage();
+	  if (isValidState(argv[2]) == 0) {
+	       printf("The state is not valid.\n");
+	       return(EXIT_FAILURE);
+	  }
+
+	  int val = atoi(argv[3]);
+
+
+     } else if (argv[1][0] == '3') {
+
+     } else {
+          printf("The stage number is not valid.\n");
 	  return(EXIT_FAILURE);
      }
-
-     if ((argv[1][0] != '1') && (argv[1][0] != '2') && (argv[1][0] != '3')) {
-          printf("Incorrect stage value.\n");
-          return(EXIT_FAILURE);
-     } 
-     
-     if (isValidState(argv[2]) == 0) {
-          printf("There is an error in your state input.\n\n");
-	  return(EXIT_FAILURE);
-     }
-
-     if (isValidMoveList(argv[3]) == 0) {
-          printf("The Move list is not valid.\n");
-          return(EXIT_FAILURE);
-     }
-
-     processMoveList(argv[2], argv[3]);
+     //MoveTree * f = MoveTree_create(argv[2],argv[3]);
+     //MoveTree_destroy(f);
 
      return (EXIT_SUCCESS);
 }
